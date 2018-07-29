9cd1117583d2abf5b39893024080ee58babce032
saachisubramaniam <saachi_subramaniam@yahoo.com>
2018-07-29 11:05:50 +0900

    Add list of articles

--- src/main/java/webanalyzer/crawler/model/CnnCrawlerModel.java
+++ src/main/java/webanalyzer/crawler/model/CnnCrawlerModel.java
@@ -7,11 +7,16 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.SocketTimeoutException;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class CnnCrawlerModel {
+
+    Map<String, Article> cache  = new ConcurrentHashMap();
 
     public List<Article> crawl(String url) throws IOException {
         List<Article> articles = new LinkedList();
@@ -21,12 +26,20 @@ public class CnnCrawlerModel {
             Elements newsHeadlines = text.select("a");
             for (int i = 0; i < newsHeadlines.size(); i++) {
                 if (newsHeadlines.get(i).attr("href").contains("news") && !newsHeadlines.get(i).attr("href").contains("http")) {
-                    System.out.println("https://www.bbc.com" + newsHeadlines.get(i).attr("href"));
-                    Document child = Jsoup.connect("https://www.bbc.com" + newsHeadlines.get(i).attr("href")).get();
+                    String articleUrl = "https://www.bbc.com" + newsHeadlines.get(i).attr("href");
+                    System.out.println(articleUrl);
+                    
+                    //if we dont have in the cache if(cache.get("URL"))
+                    Document child = Jsoup.connect(articleUrl).get();
                     System.out.println(child.select("img"));
-                    articles.add(new Article(child.text(),"https://www.bbc.com" + newsHeadlines.get(i).attr("href")));
+                    Article article = new Article(child.text(),articleUrl);
+                    cache.put(article.articleUrl, article);
+                    articles.add(article);
+                    //if we have in the cache execute below else {
+                    // articles.add(cache.get("URL");
                 }
             }
+
         } catch (SocketTimeoutException e) {
             System.out.println("err");
         }
